3363ed02ce0354cbd4e4c5b0e2d73ab21dbc3338
Roberval Filho <rslfilho@gmail.com>
2021-09-09 16:06:49 -0300

    Exercícios de fixação dia 20.2

---
+++ bloco_20/dia_2/count.sql
@@ -0,0 +1,10 @@
+SELECT * FROM sakila.staff;
+
+SELECT COUNT(password) FROM sakila.staff;
+-- 01 senha
+
+SELECT COUNT(*) FROM sakila.staff;
+-- 02 staffs
+
+SELECT COUNT(email) FROM sakila.staff;
+-- 02 emails
---
+++ bloco_20/dia_2/offset.sql
@@ -0,0 +1,5 @@
+# Query + LIMIT quantidade_de_linhas OFFSET quantidade_de_linhas
+SELECT * FROM sakila.rental LIMIT 10 OFFSET 3;
+
+# Fixação
+SELECT * FROM sakila.actor LIMIT 10 OFFSET 4;
