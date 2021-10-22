ce43bf95fbeb44014122bbc3bbcf5fa4b1f5a2cb
Roberval Filho <rslfilho@gmail.com>
2021-10-22 17:15:37 -0300

    Deixando as chamadas das funções internas aos test

--- bloco_25/dia_3/tests/exercise1-3.test.js
+++ bloco_25/dia_3/tests/exercise1-3.test.js
@@ -3,37 +3,37 @@ const { analiseNumero } = require("..");
 
 describe('Testando analiseNumero()', () => {
   describe('Quando o número passado for maior que 0', () => {
-    const retorno = analiseNumero(3);
-
     it('o retorno deve ser POSITIVO', () => {
+      const retorno = analiseNumero(3);
       expect(retorno).to.be.equal('Positivo');
     });
 
     it('o retorno deve ser to tipo string', () => {
+      const retorno = analiseNumero(3);
       expect(retorno).to.be.a('string');
     });
   });
 
   describe('Quando o número passado for menor que 0', () => {
-    const retorno = analiseNumero(-4);
-
     it('o retorno deve ser NEGATIVO', () => {
+      const retorno = analiseNumero(-4);
       expect(retorno).to.be.equal('Negativo');
     });
 
     it('o retorno deve ser to tipo string', () => {
+      const retorno = analiseNumero(-4);
       expect(retorno).to.be.a('string');
     });
   });
 
   describe('Quando o número passado for igual a 0', () => {
-    const retorno = analiseNumero(0);
-
     it('o retorno deve ser NEUTRO', () => {
+      const retorno = analiseNumero(0);
       expect(retorno).to.be.equal('Neutro');
     });
 
     it('o retorno deve ser to tipo string', () => {
+      const retorno = analiseNumero(0);
       expect(retorno).to.be.a('string');
     });
   });
--- bloco_25/dia_3/tests/exercise4-5.test.js
+++ bloco_25/dia_3/tests/exercise4-5.test.js
@@ -2,13 +2,14 @@ const { expect } = require("chai");
 const { escrevendoConteudo } = require("..");
 
 describe('Testando se escrevendoConteudo()', () => {
-  const resposta = escrevendoConteudo('arquivo.txt', 'Eu sou um arquivo.');
   
   it('após escrever o arquivo, retorna "Ok"', () => {
+    const resposta = escrevendoConteudo('arquivo.txt', 'Eu sou um arquivo.');
     expect(resposta).to.be.equal('Ok');
   });
 
   it('o retorno é uma string', () => {
+    const resposta = escrevendoConteudo('arquivo.txt', 'Eu sou um arquivo.');
     expect(resposta).to.be.a('string');
   });
 });
