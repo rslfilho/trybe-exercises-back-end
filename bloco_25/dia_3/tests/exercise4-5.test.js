9f6dadfb62894c9d7942dd57d338b35944e5b5cf
Roberval Filho <rslfilho@gmail.com>
2021-10-22 17:55:38 -0300

    mock de writeFyle

--- bloco_25/dia_3/tests/exercise4-5.test.js
+++ bloco_25/dia_3/tests/exercise4-5.test.js
@@ -1,7 +1,16 @@
 const { expect } = require("chai");
 const { escrevendoConteudo } = require("..");
+const fs = require('fs');
+const { stub, restore } = require('sinon');
 
 describe('Testando se escrevendoConteudo()', () => {
+  before(() => {
+    stub(fs, 'writeFileSync').returns(undefined);
+  });
+
+  after(() => {
+    fs.writeFileSync.restore();
+  });
   
   it('após escrever o arquivo, retorna "Ok"', () => {
     const resposta = escrevendoConteudo('arquivo.txt', 'npm i -D mocha chai sinon nyc @types/mocha @types/sinon-chai');
