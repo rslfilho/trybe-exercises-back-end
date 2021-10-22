f99e95923d5b5c75e7e3c05227c31cee7f1d5f24
Roberval Filho <rslfilho@gmail.com>
2021-10-22 17:37:37 -0300

    Mudando conteudo do arquivo no teste 4-5

--- bloco_25/dia_3/tests/exercise4-5.test.js
+++ bloco_25/dia_3/tests/exercise4-5.test.js
@@ -4,12 +4,12 @@ const { escrevendoConteudo } = require("..");
 describe('Testando se escrevendoConteudo()', () => {
   
   it('após escrever o arquivo, retorna "Ok"', () => {
-    const resposta = escrevendoConteudo('arquivo.txt', 'Eu sou um arquivo.');
+    const resposta = escrevendoConteudo('arquivo.txt', 'npm i -D mocha chai sinon nyc @types/mocha @types/sinon-chai');
     expect(resposta).to.be.equal('Ok');
   });
 
   it('o retorno é uma string', () => {
-    const resposta = escrevendoConteudo('arquivo.txt', 'Eu sou um arquivo.');
+    const resposta = escrevendoConteudo('arquivo.txt', 'npm i -D mocha chai sinon nyc @types/mocha @types/sinon-chai');
     expect(resposta).to.be.a('string');
   });
 });
