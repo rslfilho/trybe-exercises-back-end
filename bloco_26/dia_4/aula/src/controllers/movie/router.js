9cfc550323af71a3aa806def11df4b6490af45f8
Roberval Filho <rslfilho@gmail.com>
2021-11-18 11:57:31 -0300

    Finalizando config de rota

--- bloco_26/dia_4/aula/src/controllers/movie/router.js
+++ bloco_26/dia_4/aula/src/controllers/movie/router.js
@@ -5,5 +5,6 @@ const movieController = require('./index');
 const router = express.Router({ mergeParams: true });
 
 router.post('/', movieController.create);
+router.get('/:id', movieController.getById);
 
 module.exports = router;
